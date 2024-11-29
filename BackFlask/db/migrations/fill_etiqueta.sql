181eff52c1c8e69b73e22ca4eb8f51d92fbcf129
20220109Sebas <20220109@aloe.ulima.edu.pe>
2024-11-29 17:54:54 -0500

    Delete BackFlask/db/migrations/fill_etiqueta.sql

--- BackFlask/db/migrations/fill_etiqueta.sql
+++
@@ -1,9 +0,0 @@
--- migrate:up
-INSERT INTO etiquetas (id, nombre) VALUES (1, 'Urgente');
-INSERT INTO etiquetas (id, nombre) VALUES (2, 'Bug');
-INSERT INTO etiquetas (id, nombre) VALUES (3, 'Mejora');
-INSERT INTO etiquetas (id, nombre) VALUES (4, 'Producción');
-INSERT INTO etiquetas (id, nombre) VALUES (5, 'Documentación');
-
--- migrate:down
-DELETE FROM etiquetas;
