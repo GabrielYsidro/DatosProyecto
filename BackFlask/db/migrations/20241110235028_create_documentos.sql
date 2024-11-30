4073cb6ff4312091a008ce9feb628f1d91f6a239
Gabriel Walter Ysidro Palomino <gawayspa@gmail.com>
2024-11-30 12:22:14 -0500

    cambios db

--- BackFlask/db/migrations/20241110235028_create_documentos.sql
+++ BackFlask/db/migrations/20241110235028_create_documentos.sql
@@ -1,7 +1,8 @@
 -- migrate:up
 CREATE TABLE documentos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    url_doc VARCHAR(200)
+    url_doc VARCHAR(200),
+    tipo VARCHAR(30)
     );
 -- migrate:down
 DROP TABLE documentos;
--- BackFlask/db/migrations/20241111002834_create_incidencia_cliente.sql
+++ BackFlask/db/migrations/20241111002834_create_incidencia_cliente.sql
@@ -2,9 +2,9 @@
 CREATE TABLE incidencia_cliente (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     id_incidencia INTEGER NOT NULL,
-    id_usuario INTEGER NOT NULL,
+    id_cliente INTEGER NOT NULL,
     FOREIGN KEY (id_incidencia) REFERENCES incidencias(id),
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+    FOREIGN KEY (id_cliente) REFERENCES usuarios_cliente(id)
 );
 -- migrate:down
 DROP TABLE incidencia_cliente;
