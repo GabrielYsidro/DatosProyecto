b957d15bc00822a6b4db6881e9723ea3e512b726
20220109Sebas <20220109@aloe.ulima.edu.pe>
2024-11-30 13:14:33 -0500

    Update 20241129010544_fill_documentos.sql

--- BackFlask/db/migrations/20241129010544_fill_documentos.sql
+++ BackFlask/db/migrations/20241129010544_fill_documentos.sql
@@ -1,7 +1,22 @@
-INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc1.pdf', 'comentario');
-INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc2.pdf', 'incidencia'); 
-INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc3.pdf', 'comentario'); 
-INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc4.pdf', 'incidencia');
-INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc5.pdf', 'comentario'); 
-INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc6.pdf', 'incidencia'); 
+
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/incidencia1.pdf', 'incidencia'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/incidencia2.pdf', 'incidencia'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/incidencia3.pdf', 'incidencia'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/incidencia4.pdf', 'incidencia'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/incidencia5.pdf', 'incidencia'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/incidencia6.pdf', 'incidencia'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/incidencia7.pdf', 'incidencia');
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/incidencia8.pdf', 'incidencia'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/incidencia9.pdf', 'incidencia'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/incidencia10.pdf', 'incidencia'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/comentario1.pdf', 'comentario'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/comentario2.pdf', 'comentario'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/comentario3.pdf', 'comentario'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/comentario4.pdf', 'comentario'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/comentario5.pdf', 'comentario'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/comentario6.pdf', 'comentario');
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/comentario7.pdf', 'comentario'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/comentario8.pdf', 'comentario');
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/comentario9.pdf', 'comentario');
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/comentario10.pdf', 'comentario'); 
 
