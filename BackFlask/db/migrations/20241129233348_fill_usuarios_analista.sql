730b36830fca1616786504be6f132bfe4e255c71
20220109Sebas <20220109@aloe.ulima.edu.pe>
2024-11-30 12:54:47 -0500

    Update 20241129233348_fill_usuarios_analista.sql

--- BackFlask/db/migrations/20241129233348_fill_usuarios_analista.sql
+++ BackFlask/db/migrations/20241129233348_fill_usuarios_analista.sql
@@ -1,26 +1,8 @@
 -- migrate:up
--- Insertar 20 registros en la tabla `usuarios_analista`
-INSERT INTO usuarios_analista (id_usuario) VALUES (1);
-INSERT INTO usuarios_analista (id_usuario) VALUES (2);
-INSERT INTO usuarios_analista (id_usuario) VALUES (3);
-INSERT INTO usuarios_analista (id_usuario) VALUES (4);
-INSERT INTO usuarios_analista (id_usuario) VALUES (5);
-INSERT INTO usuarios_analista (id_usuario) VALUES (6);
-INSERT INTO usuarios_analista (id_usuario) VALUES (7);
-INSERT INTO usuarios_analista (id_usuario) VALUES (8);
-INSERT INTO usuarios_analista (id_usuario) VALUES (9);
-INSERT INTO usuarios_analista (id_usuario) VALUES (10);
-INSERT INTO usuarios_analista (id_usuario) VALUES (11);
-INSERT INTO usuarios_analista (id_usuario) VALUES (12);
-INSERT INTO usuarios_analista (id_usuario) VALUES (13);
-INSERT INTO usuarios_analista (id_usuario) VALUES (14);
-INSERT INTO usuarios_analista (id_usuario) VALUES (15);
-INSERT INTO usuarios_analista (id_usuario) VALUES (16);
-INSERT INTO usuarios_analista (id_usuario) VALUES (17);
-INSERT INTO usuarios_analista (id_usuario) VALUES (18);
-INSERT INTO usuarios_analista (id_usuario) VALUES (19);
-INSERT INTO usuarios_analista (id_usuario) VALUES (20);
+INSERT INTO usuarios_analista (id_usuario) VALUES (100);
+INSERT INTO usuarios_analista (id_usuario) VALUES (200);
+INSERT INTO usuarios_analista (id_usuario) VALUES (300);
 
 -- migrate:down
--- Borrar la tabla 'usuarios_analista'
+
 DROP TABLE usuarios_analista;
