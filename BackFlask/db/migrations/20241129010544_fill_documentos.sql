0b6d93bb6540d6e730c78c3f7a0e088668489827
20220109Sebas <20220109@aloe.ulima.edu.pe>
2024-11-30 13:11:13 -0500

    Update 20241129010544_fill_documentos.sql

--- BackFlask/db/migrations/20241129010544_fill_documentos.sql
+++ BackFlask/db/migrations/20241129010544_fill_documentos.sql
@@ -1,1 +1,7 @@
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc1.pdf', 'comentario');
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc2.pdf', 'incidencia'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc3.pdf', 'comentario'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc4.pdf', 'incidencia');
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc5.pdf', 'comentario'); 
+INSERT INTO documentos (url_doc, tipo_doc) VALUES ('http://example.com/doc6.pdf', 'incidencia'); 
 
