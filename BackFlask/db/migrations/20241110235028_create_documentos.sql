867720564c14ff4c4ef87678c1d57a2e75cef04f
20220109Sebas <20220109@aloe.ulima.edu.pe>
2024-11-30 13:01:31 -0500

    Update 20241110235028_create_documentos.sql

--- BackFlask/db/migrations/20241110235028_create_documentos.sql
+++ BackFlask/db/migrations/20241110235028_create_documentos.sql
@@ -2,7 +2,7 @@
 CREATE TABLE documentos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     url_doc VARCHAR(200),
-	tipo_doc VARCHAR(20)
+    tipo_doc VARCHAR(20)
     );
 -- migrate:down
 DROP TABLE documentos;
