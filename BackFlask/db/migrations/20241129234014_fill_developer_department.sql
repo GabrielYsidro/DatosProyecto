cacd5de2d582f4d9dc0d8a123ba655cf570d4e01
20220109Sebas <20220109@aloe.ulima.edu.pe>
2024-11-30 19:12:54 -0500

    Update 20241129234014_fill_developer_department.sql

--- BackFlask/db/migrations/20241129234014_fill_developer_department.sql
+++ BackFlask/db/migrations/20241129234014_fill_developer_department.sql
@@ -1,63 +1,30 @@
 -- migrate:up
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (1, 1);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (2, 2);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (3, 3);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (4, 4);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (5, 5);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (6, 6);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (7, 7);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (8, 8);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (9, 9);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (10, 10);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (11, 1);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (12, 2);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (13, 3);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (14, 4);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (15, 5);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (16, 6);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (17, 7);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (18, 8);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (19, 9);
-
-INSERT INTO developer_departamentos (id_developer, id_departamento)
-VALUES (20, 10);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (1, 2);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (2, 3);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (3, 3);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (3, 4);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (3, 1);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (3, 5);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (4, 2);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (4, 1);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (4, 3);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (5, 4);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (5, 5);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (5, 2);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (5, 1);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (5, 3);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (6, 4);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (6, 3);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (6, 5);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (7, 1);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (8, 1);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (9, 1);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (9, 4);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (10, 1);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (10, 5);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (10, 2);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (10, 4);
+INSERT INTO developer_departamentos (id_developer, id_departamento) VALUES (10, 3);
 
 -- migrate:down
-DROP TABLE developer_departamentos;
+DELETE FROM developer_departamentos;
